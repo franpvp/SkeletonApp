2771b83a7f0611ea96de2966387f74a5a83f42de
franpvp <franpvp.98@gmail.com>
2023-05-27 20:29:03 -0400

    Se solucionan errores de ngModel

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LoginPage } from '../login/login.page';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  data: any;
 
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    if (navigation?.extras.state && 'user' in navigation.extras.state) {
+      this.data = navigation.extras.state['user'];
+      console.log(this.data);
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  toLogin(){
+    this.router.navigateByUrl('/login');
+  }
 }
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  user={
+    usuario:"",
+    password:""
+  }
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  ingresar(){
+    let navigationExtras: NavigationExtras = {
+      state: {
+        user: this.user
+      }
+    };
+    this.router.navigate(['/home'],navigationExtras);
+  }
+
+  
+  
+
+}
